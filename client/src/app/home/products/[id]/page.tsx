54e561fd4d80a3e431b3851b363e28e8b192c502
Thái Văn Duy <77010116+duyn12@users.noreply.github.com>
2024-06-03 15:08:20 +0700

    update product details

--- client/src/app/home/products/[id]/page.tsx
+++ client/src/app/home/products/[id]/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+
 import productApiRequest from "@/apiRequests/product";
 import Image from "next/image";
 import { cache } from "react";
@@ -10,6 +11,14 @@ import { cookies } from "next/headers";
 import { Metadata, ResolvingMetadata } from "next";
 import * as React from "react";
 import { Minus, Plus } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const getDetail = cache(productApiRequest.getDetail);
 type Props = {
@@ -20,15 +29,14 @@ export async function generateMetadata(
   { params }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  const { payload } = await getDetail(Number(params.id))
-  const product = payload.data
+  const { payload } = await getDetail(Number(params.id));
+  const product = payload.data;
   return {
     title: product.name,
-    description: product.description
-  }
+    description: product.description,
+  };
 }
 export default async function ProductDetail({ params }: Props) {
-  
   const cookieStore = cookies();
   const sessionToken = cookieStore.get("sessionToken");
   const isAuthenticated = Boolean(sessionToken);
@@ -36,7 +44,6 @@ export default async function ProductDetail({ params }: Props) {
   try {
     const { payload } = await getDetail(Number(params.id));
     product = payload.data;
-    
   } catch (error) {}
   // const url = envConfig.NEXT_PUBLIC_URL + '/products/' + product.id
   const iconProduct = [
@@ -52,11 +59,11 @@ export default async function ProductDetail({ params }: Props) {
       maximumFractionDigits: 0, // Omitting decimal values for VND as they are generally not used
     }).format(value);
   }
-  // const [quantity, setQuantity] = useState(1);
+  // const decrementQuantity = () => {
+  //   quantity > 1 ? getDetail(id, { quantity: quantity - 1}).then(handleUpdateCart) : removeItem();
 
-  // const onClick = (amount: number) => {
-  //   setQuantity((prevQuantity: number) => prevQuantity + amount);
-  // };
+  // }
+
   return (
     <div className="pt-6">
       {!product && <div>Không tìm thấy sản phẩm</div>}
@@ -64,42 +71,77 @@ export default async function ProductDetail({ params }: Props) {
         <div className="mx-auto max-w-screen-xl">
           <ProductBreadcrumb homeElement={undefined} separator={undefined} />
           <div className="mx-auto flex my-16">
-            <div className="product-images">
-              <Image
-                src={product.img}
-                alt={product.name}
-                width={480}
-                height={480}
-                className="max-h-[480px] rounded-lg"
-              />
-            </div>
-            <div className="lg:ml-6 lg:py-6 lg:p-10 rounded-3xl bg-white">
-              <h1 className="text-gray-900 text-3xl mb-12">{product.name}</h1>
-              <p className="text-sm my-4">{product.description}</p>
-              <div className="flex items-center justify-center space-x-2">
+            <div className="w-2/5">
+              <div className="product-images mb-4">
+                <Image
+                  src={product.img}
+                  alt={product.name}
+                  width={480}
+                  height={480}
+                  className="max-h-[480px] rounded-lg"
+                />
               </div>
-              <span className="font-bold text-2xl text-red-600">
-                {formatVND(product.price)}
-              </span>
-              <div className="flex my-6 gap-6">
-                <Link href={`/home/cart`} className="w-full">
-                  <Button variant="secondary" className="flex ml-auto lg:w-1/2">
-                    <ShoppingCart className="mr-2" />
-                    Thêm vào giỏ hàng
-                  </Button>
-                </Link>
-
-                {isAuthenticated && (
-                  //  <Link href={`/home/checkout/${product.id}`}></Link>
-                  <Link href={`/home/cart`}>
-                    <Button
-                      variant="destructive"
-                      className="flex ml-auto w-32  w-full"
+              <Carousel className="w-full max-w-sm">
+                <CarouselContent className="-ml-1">
+                  {Array.from({ length: 7 }).map((_, index) => (
+                    <CarouselItem
+                      key={index}
+                      className="pl-1 md:basis-1/2 lg:basis-1/4"
                     >
-                      Mua ngay
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex aspect-square items-center justify-center p-6">
+                            <span className="text-2xl font-semibold">
+                              {index + 1}
+                            </span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+              </Carousel>
+            </div>
+            <div className="lg:ml-6 lg:p-12 rounded-3xl bg-white">
+              <div>
+                <h1 className="text-gray-900 text-3xl mb-12">{product.name}</h1>
+                <p className="text-sm my-4">{product.description}</p>
+                <div className="flex items-center space-x-2">
+                  <Button>
+                    <Minus className="w-4 h-4" />
+                  </Button>
+                  <span className="font-bold text-xl">1</span>
+                  <Button>
+                    <Plus className="w-4 h-4" />
+                  </Button>
+                </div>
+              </div>
+              <div className="grid grid-cols-2 h-36 content-end space-x-2">
+                <span className="font-bold text-2xl text-red-600">
+                  {formatVND(product.price)}
+                </span>
+                <div className="flex  my-6 gap-6">
+                  <Link href={`/home/cart`} className="">
+                    <Button variant="secondary" className="flex ml-auto ">
+                      <ShoppingCart className="mr-2 w-5 h-5" />
+                      Thêm vào giỏ hàng
                     </Button>
                   </Link>
-                )}
+
+                  {isAuthenticated && (
+                    //  <Link href={`/home/checkout/${product.id}`}></Link>
+                    <Link href={`/home/cart`}>
+                      <Button
+                        variant="destructive"
+                        className="flex ml-auto w-32 "
+                      >
+                        Mua ngay
+                      </Button>
+                    </Link>
+                  )}
+                </div>
               </div>
             </div>
           </div>
@@ -128,17 +170,11 @@ export default async function ProductDetail({ params }: Props) {
               <li className="text-slate-50">{product.description}</li>
             </ul>
             <div className="flex justify-start gap-x-4 pt-4">
-              <Button
-                variant="destructive"
-                className="w-32 "
-              >
+              <Button variant="destructive" className="w-32 ">
                 Cấu hình
               </Button>
-              <Button
-                variant="destructive"
-                className="w-32 "
-              >
-               Mua hàng
+              <Button variant="destructive" className="w-32 ">
+                Mua hàng
               </Button>
             </div>
             {/* <p className="text-white"> </p> */}
